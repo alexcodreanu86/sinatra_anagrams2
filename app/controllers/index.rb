40899266489df671e5c29fa6bf48b3b1f3add7c8
Apprentice <apprentice@dbc-chicago-029-34.local>
2014-02-17 15:12:33 -0600

    working on the anagrams

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,11 @@
 get '/' do
   # Look in app/views/index.erb
-  text =
+  @text = params[:argument]
   erb :index
+end
+
+get '/:word' do
+  @word = params[:word]
+  "Word: #{@word}<br> Anagrams:"
+  # "Word: #{@word}\nAnagrams:#{@word.anagrams}"
 end
